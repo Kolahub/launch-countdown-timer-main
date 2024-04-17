022e27176226c4d1543da685f466a44279c35b22
Ayofe Faheez <ayofefaheez@gmail.com>
2024-04-17 15:02:05 -0800

    moblie  responsive

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// const timerContent = document.getElementById('timer')
-let numDay = document.querySelector('.num-day')
-let numHrs = document.querySelector('.num-hrs')
-let numMin = document.querySelector('.num-mins')
-let numSec = document.querySelector('.num-sec')
+const numDay = document.querySelector('.num-day')
+const numHrs = document.querySelector('.num-hrs')
+const numMin = document.querySelector('.num-mins')
+const numSec = document.querySelector('.num-sec')
 const timeDayBox = document.querySelector('.timer-day')
 const timeHrsBox = document.querySelector('.timer-hrs')
 const timeMinsBox = document.querySelector('.timer-mins')
 const timeSecBox = document.querySelector('.timer-sec')
-numDay.textContent = '';
-numHrs.textContent = '';
-numMin.textContent = '';
-numSec.textContent = '';
-let timeDay = 8
+let timeDay = 8;
 let timeHour = 23
-let timeMin = 55;
-let timeSec = 41;
+let timeMin = 1;
+let timeSec = 3;
 const startTimer = setInterval(function () {
     timeSec--
     timeSecBox.classList.add('anime')
@@ -24,20 +19,20 @@ const startTimer = setInterval(function () {
     const sec = String(timeSec).padStart(2, 0)
 
     if (timeSec === 0) {
-        timeSec = 60;
         timeMin--
-        timeMinsBox.classList.add('animeOnce')
+        timeSec = 60;
+        timeMinsBox.classList.add('anime')
         setTimeout(function() {
-        timeMinsBox.classList.remove('animeOnce')
+        timeMinsBox.classList.remove('anime')
         }, 1000)
     } 
-    if (timeMin === 0) {
+    if (timeMin < 0) {
         timeSec = 60;
         timeMin = 59;
         timeHour--
-        timeHrsBox.classList.add('animeOnce')
+        timeHrsBox.classList.add('anime')
         setTimeout(function() {
-            timeHrsBox.classList.remove('animeOnce')
+            timeHrsBox.classList.remove('anime')
             }, 1000)
     }
     if (timeHour === 0) {
@@ -45,9 +40,9 @@ const startTimer = setInterval(function () {
         timeMin = 59;
         timeHour = 24
         timeDay--
-        timeDayBox.classList.add('animeOnce')
+        timeDayBox.classList.add('anime')
         setTimeout(function() {
-            timeDayBox.classList.remove('animeOnce')
+            timeDayBox.classList.remove('anime')
             }, 1000)
     } 
     if (timeDay === 0) {
@@ -55,7 +50,7 @@ const startTimer = setInterval(function () {
         timeMin = 59;
         timeHour = 24
     }
-    if(timeDay === 0 && timeHour === 0 && timeMin === 0 && timeSec === 0) (clearInterval(startTimer))
+    if(timeDay === 0 && timeHour === 24 && timeMin === 59 && timeSec === 60) (clearInterval(startTimer))
 
     numDay.textContent = day;
     numHrs.textContent = hrs;
